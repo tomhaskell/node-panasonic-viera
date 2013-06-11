e41930c51d9ea2f8017842d72dc8ba92fef6e51c
THLabs <tom@thlabs.net>
2013-06-11 21:31:43 +0100

    updated index.js to send command line input to TV

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ var PanasonicViera = require('./panasonicviera');
 
 var tv = new PanasonicViera('192.168.1.101');
 
-tv.send(PanasonicViera.MUTE_TOGGLE);
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+read = function(){
+	rl.question("Command: ", function(command) {
+		if(command == ''){
+			rl.close();
+		}else{
+	  		tv.send(command);
+	  		read();
+	  	}
+	});
+}
+
+read();
